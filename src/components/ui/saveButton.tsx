71d51b5998351362ad536b041112081c4f583d35
Siddharth Choudhary <csiddharth380@gmail.com>
2025-01-18 19:48:51 +0530

    Added save button

--- src/components/ui/saveButton.tsx
+++ src/components/ui/saveButton.tsx
@@ -1,24 +1,19 @@
 "use client";
 
 import { Save } from "lucide-react";
-import { useState } from "react";
 
 interface SaveButtonProps {
   onClick: () => void;
 }
 
 export default function SaveButton({ onClick }: SaveButtonProps) {
-  const [clicked, setClicked] = useState(false);
   return (
     <div className="fixed top-24 right-5">
       <button
-        className={`text-white transition-all duration-300 hover:scale-110 active:scale-90 p-4 ${
-          clicked ? "bg-green-500  rounded-full" : " bg-red-600 rounded-full"
-        }`}
+        className="text-white  bg-black transition-all duration-300 hover:scale-110 active:scale-90 p-4 rounded-full shadow-md  shadow-cyan-400"
         aria-label="Share"
         onClick={() => {
-          setClicked(true);
-          onClick;
+          onClick();
         }}
       >
         <Save className="text-white w-6 h-6" />
